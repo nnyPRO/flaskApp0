38b1ace093f2a601bbaad4d27a71974ca1d5ae73
nnyPRO <nanmesanee.17@gmail.com>
2023-11-25 00:34:45 +0700

    new

--- app/views.py
+++ app/views.py
@@ -1,27 +1,9 @@
-from flask import jsonify
+# from flask import jsonify
 from app import app
-from app import hw_views
+# from app import hw_views
 
 
 
-
-@app.route('/')
-def home():
-    return "Flask says 'Hello world!'"
-
-
-
-@app.route('/phonebook')
-def index():
-    return app.send_static_file('phonebook.html')
-
-
-# This route serves the dictionary d at the route /data
-@app.route("/api/data")
-def data():
-    # define some data
-    d = {
-        "Alice": "(708) 727-2377",
-        "Bob": "(305) 734-0429"
-    }
-    return jsonify(d)  # convert your data to JSON and return
+@app.route('/lab02')
+def resume():
+    return app.send_static_file('lab02_resume.html')
