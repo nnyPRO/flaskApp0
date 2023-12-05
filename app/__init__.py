320465173b148bf54efde8afe09fbcd5ef6d93c1
nnyPRO <nanmesanee.17@gmail.com>
2023-12-06 01:01:27 +0700

    Modifiled

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 app = Flask(__name__, static_folder='static')
 
+app.jinja_options = app.jinja_options.copy()
+app.jinja_options.update({
+    'trim_blocks': True,
+    'lstrip_blocks': True
+})
+
 app.config['DEBUG'] = True
 
 from app import views # noqa
+
